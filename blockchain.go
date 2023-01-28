0308e769731d2a915a6ce08e65359dccaff07e9b
Elar Saks <elarsaks@gmail.com>
2023-01-28 18:32:39 +0200

    Comment code.

--- blockchain.go
+++ blockchain.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+//Define Block type
 type Block struct {
 	nonce        int
 	previousHash string
@@ -14,6 +15,7 @@ type Block struct {
 	timeStamp    int64
 }
 
+// Function to create a new block
 func NewBlock(nonce int, previousHash string) *Block {
 	b := new(Block)
 	b.nonce = nonce
@@ -22,6 +24,7 @@ func NewBlock(nonce int, previousHash string) *Block {
 	return b
 }
 
+// Function to print the block
 func (b *Block) Print() {
 	fmt.Printf("nonce           %d\n", b.nonce)
 	fmt.Printf("previous_hash   %s\n", b.previousHash)
@@ -29,23 +32,27 @@ func (b *Block) Print() {
 	fmt.Printf("time_stamp      %d\n", b.timeStamp)
 }
 
+// Define Blockchain type
 type Blockchain struct {
 	transactionPool []string
 	chain           []*Block
 }
 
+// Function to create a new blockchain
 func NewBlockchain() *Blockchain {
 	bc := new(Blockchain)
 	bc.CreateBlock(0, "Init hash")
 	return bc
 }
 
+// Function to create and add a new block to the blockchain
 func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b)
 	return b
 }
 
+// Function to print the blockchain
 func (bc *Blockchain) Print() {
 	for i, block := range bc.chain {
 		fmt.Printf("%s Chain: %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
@@ -54,10 +61,12 @@ func (bc *Blockchain) Print() {
 	fmt.Printf("%s\n", strings.Repeat("*", 25))
 }
 
+// Function to initialize the logger
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
+// Main function
 func main() {
 	blockChain := NewBlockchain()
 	blockChain.Print()
