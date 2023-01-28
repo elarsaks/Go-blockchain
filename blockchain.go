b73fbcf94c16d34281d0be09722188eadf965f3a
Elar Saks <elarsaks@gmail.com>
2023-01-28 23:52:38 +0200

    Add block hashing (broken).

--- blockchain.go
+++ blockchain.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
 	"time"
 )
 
-//Define Block type
+// Define Block type
 type Block struct {
 	nonce        int
-	previousHash string
+	previousHash [32]byte
 	transactions []string
 	timeStamp    int64
 }
 
-// Function to create a new block
-func NewBlock(nonce int, previousHash string) *Block {
+// Create new block
+func NewBlock(nonce int, previousHash [32]byte) *Block {
 	b := new(Block)
 	b.nonce = nonce
 	b.previousHash = previousHash
@@ -24,10 +26,10 @@ func NewBlock(nonce int, previousHash string) *Block {
 	return b
 }
 
-// Function to print the block
+// Print the block
 func (b *Block) Print() {
 	fmt.Printf("nonce           %d\n", b.nonce)
-	fmt.Printf("previous_hash   %s\n", b.previousHash)
+	fmt.Printf("previous_hash   %x\n", b.previousHash)
 	fmt.Printf("transactions    %s\n", b.transactions)
 	fmt.Printf("time_stamp      %d\n", b.timeStamp)
 }
@@ -38,21 +40,27 @@ type Blockchain struct {
 	chain           []*Block
 }
 
-// Function to create a new blockchain
+// Create blockchain (including genesis block)
 func NewBlockchain() *Blockchain {
+	b := &Block{}
 	bc := new(Blockchain)
-	bc.CreateBlock(0, "Init hash")
+	bc.CreateBlock(0, b.Hash())
 	return bc
 }
 
-// Function to create and add a new block to the blockchain
-func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
-	b := NewBlock(nonce, previousHash)
+// Create new block
+func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
+	b := &Block{}
 	bc.chain = append(bc.chain, b)
 	return b
 }
 
-// Function to print the blockchain
+// Get last block
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
+}
+
+// Print blockchain
 func (bc *Blockchain) Print() {
 	for i, block := range bc.chain {
 		fmt.Printf("%s Chain: %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
@@ -61,17 +69,46 @@ func (bc *Blockchain) Print() {
 	fmt.Printf("%s\n", strings.Repeat("*", 25))
 }
 
+// Generate hash for block //TODO: Learn more about this
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+
+	return sha256.Sum256([]byte(m))
+}
+
+// Marshal block to JSON //TODO: Learn more about this
+func (b *Block) MashalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		Nonce        int      `json:"nonce"`
+		PreviousHash [32]byte `json:"previous_hash"`
+		Transactions []string `json:"transactions"`
+		TimeStamp    int64    `json:"time_stamp"`
+	}{
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Transactions: b.transactions,
+		TimeStamp:    b.timeStamp,
+	})
+}
+
 // Function to initialize the logger
-func init() {
+func init() { //? TODO: Why we need this?
 	log.SetPrefix("Blockchain: ")
 }
 
 // Main function
 func main() {
+	// Initialize a new blockchain ()
 	blockChain := NewBlockchain()
 	blockChain.Print()
-	blockChain.CreateBlock(5, "hash1")
+
+	// Create a new block
+	previousHash := blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(5, previousHash)
 	blockChain.Print()
-	blockChain.CreateBlock(2, "hash2")
+
+	// Create a new block
+	previousHash = blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(2, previousHash)
 	blockChain.Print()
 }
