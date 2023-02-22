620eb5ea4a5f907a234208c55ac63a174341ab9b
Elar Saks <elarsaks@gmail.com>
2023-02-22 13:49:37 +0200

    Configure port.

--- block/blockchain_server/main.go
+++ block/blockchain_server/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 )
 
+// Function to initialize the logger
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
 func main() {
-	fmt.Println("Test")
+	port := flag.Uint("port", 5000, "TCP Port Number for Blockchain Server")
+	flag.Parse()
+	fmt.Println(*port)
 }
