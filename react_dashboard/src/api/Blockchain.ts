a75e3625e15c3ffa2f08552bf438b9b20aaf64a2
Elar Saks <elarsaks@gmail.com>
2023-07-03 17:17:15 +0300

    Switch from async to promises.

--- react_dashboard/src/api/Blockchain.ts
+++ react_dashboard/src/api/Blockchain.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { Blockchain } from "../Type";
 
-async function fetchBlockchainData(): Promise<Blockchain> {
-  try {
-    const response = await axios.get<Blockchain>("http://localhost:5001/");
-    return response.data;
-  } catch (error) {
-    console.error("Failed to fetch blockchain data:", error);
-    return { chain: [] };
-  }
+function fetchBlockchainData(): Promise<Blockchain> {
+  return axios
+    .get<Blockchain>("http://localhost:5001/")
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Failed to fetch blockchain data:", error);
+      return { chain: [] };
+    });
 }
+
 
 export { fetchBlockchainData };
