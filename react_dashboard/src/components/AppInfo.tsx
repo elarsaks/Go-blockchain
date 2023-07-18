9111854cdf5853130690a4c0ebcd70bba96cae77
Elar Saks <elarsaks@gmail.com>
2023-07-18 12:06:14 +0300

    Change wording based on screen size.

--- react_dashboard/src/components/AppInfo.tsx
+++ react_dashboard/src/components/AppInfo.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import styled from "styled-components";
+import React, { useState, useEffect } from "react";
 
 const AppInfoWrapper = styled.div`
   display: flex;
@@ -40,36 +40,54 @@ const AppInfoWrapper = styled.div`
   }
 `;
 
-const AppInfo: React.FC = () => (
-  <AppInfoWrapper>
-    <h3> This is a simple example of a blockchain.</h3>
-    <p>
-      The wallet on the left represents a miner, while the wallet on the right
-      represents a random user. Miner wallets accumulate crypto when they mine
-      blocks. At first, they mine 10 blocks, and then they mine 1 block only if
-      there is transactions to be mined..
-    </p>
-    <p>
-      You can experiment by sending this crypto from miners to users and vice
-      versa. Just copy and paste the wallet address from one wallet to the other
-      wallets recipient input.
-    </p>
-    <p>Beneath the wallets, you'll find the 10 most recently mined blocks.</p>
-    <br />
-    <p>
-      <b>
-        Check out the project on{" "}
-        <a
-          href="https://github.com/elarsaks/Go-blockchain"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>{" "}
-      </b>
-      .
-    </p>
-  </AppInfoWrapper>
-);
+const AppInfo: React.FC = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 850);
+    };
+
+    handleResize(); // Check initial width
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <AppInfoWrapper>
+      <h3> This is a simple example of a blockchain.</h3>
+      <p>
+        The wallet on the {isMobile ? "up" : "left"} represents a miner, while
+        the wallet on the {isMobile ? "down" : "right"} represents a random
+        user. Miner wallets accumulate crypto when they mine blocks. At first,
+        they mine 10 blocks, and then they mine 1 block only if there are
+        transactions to be mined.
+      </p>
+      <p>
+        You can experiment by sending this crypto from miners to users and vice
+        versa. Just copy and paste the wallet address from one wallet to the
+        other wallets recipient input.
+      </p>
+      <p>Beneath the wallets, you'll find the 10 most recently mined blocks.</p>
+      <br />
+      <p>
+        <b>
+          Check out the project on{" "}
+          <a
+            href="https://github.com/elarsaks/Go-blockchain"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+        </b>
+        .
+      </p>
+    </AppInfoWrapper>
+  );
+};
 
 export default AppInfo;
