14e6c5caed6fd3094409ada488bf5f51ed08d38b
Elar Saks <elarsaks@gmail.com>
2023-07-24 19:54:43 +0300

    Fix fetching blocks.

--- wallet_server/main.go
+++ wallet_server/main.go
@@ -205,8 +205,6 @@ func (ws *WalletServer) WalletBalance(w http.ResponseWriter, req *http.Request) 
 
 // Handler function to get requested blocks
 func (ws *WalletServer) GetBlocks(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("GetBlocks from wallet")
-
 	// Get the 'amount' query parameter from the URL
 	amountStr := req.URL.Query().Get("amount")
 	amount, err := strconv.Atoi(amountStr)
@@ -218,20 +216,27 @@ func (ws *WalletServer) GetBlocks(w http.ResponseWriter, req *http.Request) {
 	// Make a GET request to miner-2's API to fetch blocks
 	resp, err := http.Get(fmt.Sprintf(ws.Gateway()+"/miner/blocks?amount=%d", amount))
 	if err != nil {
-		http.Error(w, "Error fetching blocks from miner-2", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
 
+	// Check the response status code
+	if resp.StatusCode != http.StatusOK {
+		http.Error(w, "Error fetching blocks from miner-2", resp.StatusCode)
+		return
+	}
+
 	// Decode the JSON response into a slice of Block
 	var blocks []block.Block
 	if err := json.NewDecoder(resp.Body).Decode(&blocks); err != nil {
-		http.Error(w, "Error decoding blocks response", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	// Respond with JSON-encoded blocks
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*") // Example CORS header, customize as needed
 	json.NewEncoder(w).Encode(blocks)
 }
 
