f5ead7df35dd376da8cb3df2282cdbd117528933
Elar Saks <elarsaks@gmail.com>
2023-07-24 22:07:44 +0300

    Start adding ssh to React.

--- react_dashboard/src/api/blockchain.ts
+++ react_dashboard/src/api/blockchain.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const { REACT_APP_API_URL } = process.env;
-const WALLET_SERVER_URL = REACT_APP_API_URL
-  ? REACT_APP_API_URL
+const { REACT_APP_GATEWAY_API_URL } = process.env;
+const WALLET_SERVER_URL = REACT_APP_GATEWAY_API_URL
+  ? REACT_APP_GATEWAY_API_URL
   : "goblockchain.azurecr.io"; // During build there is no env variables
 
 function fetchBlockchainData(): Promise<[Block]> {
