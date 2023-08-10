b6f46fdf4373ce549a59fc022217b2d64d5e1f86
Elar Saks <elarsaks@gmail.com>
2023-08-10 04:15:34 +0300

    Build single wallet reducer.

--- cmd/react_dashboard/src/store/WalletReducer.ts
+++ cmd/react_dashboard/src/store/WalletReducer.ts
@@ -1,1 +1,20 @@
-export {};
+type WalletAction =
+  | { type: "SET_WALLET"; payload: Partial<StoreWallet> }
+  | { type: "SET_UTIL"; payload: UtilState };
+
+export default (state: StoreWallet, action: WalletAction): StoreWallet => {
+  switch (action.type) {
+    case "SET_WALLET":
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case "SET_UTIL":
+      return {
+        ...state,
+        util: action.payload,
+      };
+    default:
+      return state;
+  }
+};
